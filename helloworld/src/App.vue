06ecfbac06ef58d9496e64cdc3e0574233a692be
Onebrush Choi <w.hyacinth.choi@gmail.com>
2020-06-07 20:05:38 +0900

    출석체크 캘린더 샘플 업로드

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Date /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Date from './components/Date'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Date
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ helloworld/src/components/Date.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="calender">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Date',
+    props: {
+
+    },
+    data: function(){
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.calender {
+    width: 500px;
+    &:after {
+        clear: both;
+        display: block;
+        content: "";
+    }
+    ::v-deep div {
+        float: left;
+        width: 100px;
+        height: 100px;
+        font-weight: bold;
+        font-size: 20px;
+        text-align: center;
+        line-height: 100px;
+        border: 1px solid #000;
+        box-sizing: border-box;
+        &.active {
+            background: #0bf;
+        }
+    }
+}
+</style>
